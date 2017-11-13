aa3dbbbf61c10528c75fca77c6ac171beaa24617
cindy <cindy.nicolay@laposte.net>
2017-11-13 16:43:22 +0100

    plugin about me page dans la sidebar

---
+++ a-propos.php
@@ -0,0 +1,36 @@
+
+<p>test</p>
+<main id="apropos">
+
+        <section id="suiveznous">
+          <h3>SUIVEZ-NOUS !</h3>
+          <article id="social">
+            <div ><a href=""><img src="img/linkedin.png" alt="linkedin"></a><span><br>+50 000 Membres</span></div>
+            <div><a href=""><img src="img/twitter.png" alt="twitter"></a><span><br>+80 000 Followers</span></div>
+            <div><a href=""><img src="img/insta.png" alt="instagram"></a><span><br>+10 000 Abonnés</span></div>
+            <div><a href=""><img src="img/fb.png" alt="facebook"></a><span><br>+30 000 Likes</span></div>
+            <div><a href=""><img src="img/news.png" alt="newsletter"></a><span><br>+60 000 Newsletters</span></div>
+          </article>
+        </section>
+
+
+        <section id="nous">
+
+          <article id="equipe">
+            <h3>NOTRE EQUIPE</h3>
+            <div id="employe">
+              <div class="d-block"><img src="img/peppa.png" alt="peppa pig" class="rounded-circle"><span><br>Peppa Pig<br>-Rédactrice super-chef-</span></div>
+              <div class="d-block"><img src="img/rebecca.jpg" alt="rebecca rabbit" class="rounded-circle"><span><br>Rebecca Rabbit<br>-Journaliste spécialisé dans le blabla-</span></div>
+              <div class="d-block"><img src="img/suzy.jpg" alt="suzy sheep" class="rounded-circle"><span><br>Suzy Sheep<br>-Responsable des partenariats-</span></div>
+            </div>
+          </article>
+
+          <article id="adress">
+            <h3>NOTRE ADRESSE</h3>
+            <address>3 Rue du petit cochon<br>10 000<br>En haut de la coline</address>
+          </article>
+
+        </section>
+
+      </main>
+<?php get_footer(); ?>
